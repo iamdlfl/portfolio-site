446bca6356ee399f61cdd44c36ea7b67f80b235b
David Lynch <davidjlynch1017@gmail.com>
2020-11-21 08:50:45 -0500

    Removign all files in .gitingore

--- app/assets/OldSite/keys.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$googleMapsKey = "AIzaSyCVRI5kmhwfWN7_5RVJ9jzKQNDjM1WxcwQ";
-$weatherKey = "3f0578c87edd6b2e9d67bb1d648e6d2e";
-$googleGeoKey = "AIzaSyB2k4-m0biGYR2-EbFTo4W9m9EHi31T5Z8";
- ?>
